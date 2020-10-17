ce63d0f1718c0fb184f942e37a508041dc749eec
Don Lamarr <donlevans29@gmail.com>
2020-10-16 20:39:12 -0500

    configured server.js file

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 // import
+import express from 'express'
+import mongoose from 'mongoose'
+import pusher from 'pusher'
+import cors from 'cors'
+
 
 // app config
+const app = express();
+const port = process.env.PORT || 9000
+
 
 // middleware
+app.use(cors())
+app.use(express.json())
+
 
 // db config
 
+
 // api routes
+app.get('/', (req,res) => res.status(200).send('Hello From Squidux! 🦑 '))
+
 
 // listen
+app.listen(port, () => console.log(`listening on localhost:${port}`))
